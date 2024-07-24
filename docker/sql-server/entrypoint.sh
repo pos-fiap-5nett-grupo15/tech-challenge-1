259d4fe465658440e09714e7ebe779fd19dda59b
Caio Maia <caiomaia3@gmail.com>
2024-07-23 22:40:42 -0300

    create mssql custom image

---
+++ docker/sql-server/entrypoint.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+/usr/config/configure-db.sh &
+
+# start sql server
+/opt/mssql/bin/sqlservr
---
+++ docker/sql-server/setup.sql
@@ -0,0 +1,13 @@
+/*
+
+Enter custom T-SQL here that would run after SQL Server has started up. 
+
+*/
+
+CREATE DATABASE tech-1;
+GO
+
+USE tech_1;
+
+CREATE SCHEMA ContactsManagement;
+GO
